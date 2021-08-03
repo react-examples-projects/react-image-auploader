893875d713fb371c1a2dbf53911feef13fe014eb
znareak <libardojesusrengifo129@gmail.com>
2021-08-03 11:31:37 -0400

    server cors 3

--- server/index.js
+++ server/index.js
@@ -6,27 +6,26 @@ const helmet = require("helmet");
 const hpp = require("hpp");
 const cors = require("cors");
 const rateLimit = require("express-rate-limit");
-
 const app = express();
 const routers = require("./routers");
 const startServer = require("./config/server");
-
-// Security middlewares
-app.use(cors({ origin: "*" }));
-app.use(helmet());
-app.use(hpp());
-app.use(
-  rateLimit({
-    windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 50, // limit each IP to 100 requests per windowMs
-  })
-);
 
 app.use(morgan("dev"));
 app.use(express.static("./uploads"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+
+// Security middlewares
+app.use(helmet());
+app.use(hpp());
+app.use(cors());
+app.use(
+  rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 50, // limit each IP to 100 requests per windowMs
+  })
+);
 
 app.use("/api", routers);
 startServer(app);
